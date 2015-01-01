9b39e1c5fdcdd15cf3b77160529fdcbb3d2eb203
Ricky Yu <rickyyu2294@gmail.com>
2015-01-01 02:09:17 -0800

    Make a Toast if User Hasn't Filled Out All Fields Of Character Edit Activity

--- app/src/main/java/com/ricky/encounterassistant/ui/activities/CharacterEditActivity.java
+++ app/src/main/java/com/ricky/encounterassistant/ui/activities/CharacterEditActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ricky.encounterassistant.R;
 import com.ricky.encounterassistant.models.*;
@@ -18,6 +19,7 @@ import java.util.UUID;
  * Created by Ricky on 12/29/2014.
  */
 public class CharacterEditActivity extends Activity {
+    private static final String TAG = "characterEditActivity";
     public static final String EXTRA_NAME = "com.ricky.encounterassistant.name";
     public static final String EXTRA_MAX_HEALTH = "com.ricky.encounterassistant.maxhealth";
     public static final String EXTRA_INITIATIVE = "com.ricky.encounterassistant.initiative";
@@ -54,6 +56,13 @@ public class CharacterEditActivity extends Activity {
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (nameEditText.getText().toString().equals("") || maxHealthEditText.getText().toString().equals("") ||
+                        initiativeEditText.getText().toString().equals("") || acEditText.getText().toString().equals("")) {
+                    Toast toast = Toast.makeText(CharacterEditActivity.this, "Fill Out All Fields", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+
                 Intent intent = new Intent();
                 intent.putExtra(EXTRA_NAME, nameEditText.getText().toString());
                 intent.putExtra(EXTRA_MAX_HEALTH, Integer.parseInt(maxHealthEditText.getText().toString()));
